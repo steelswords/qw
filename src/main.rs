783e7f394b9b51b81ca97dbe33caeb1f66a69a11
Tristan Andrus <tristan@tristanandrus.com>
2023-09-26 21:00:02 -0600

    Files display in root directory
    Signed-off-by: Tristan Andrus <tristan@tristanandrus.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use] extern crate rocket;
 use std::path::PathBuf;
+use std::fs;
 
 use rocket::{fs::TempFile, figment::providers::Format};
 use rocket::form::Form;
@@ -26,6 +27,27 @@ struct Breadcrumb {
     pub path: String,
 }
 
+fn get_all_files_in_directory(parent_path: PathBuf) -> Vec<LocalFile> {
+    println!("parent_path = {:?}", parent_path);
+    let paths = fs::read_dir(parent_path).unwrap_or(fs::read_dir(PathBuf::from(".")).unwrap());
+    let mut return_vec = vec![];
+
+    for path in paths {
+        if let Ok(path) = path {
+            println!("Name: {}", path.path().display());
+            return_vec.push(
+                LocalFile {
+                    // TODO
+                    path_type: "File".to_string(),
+                    // TODO
+                    path: String::new(),
+                    name: path.file_name().into_string().expect("Could not stringify file name"),
+                }
+            );
+        }
+    }
+    return_vec
+}
 
 #[post("/<path..>", data = "<form>")]
 async fn upload(path: PathBuf, mut form: Form<Upload<'_>>) -> std::io::Result<()> {
@@ -42,18 +64,21 @@ async fn upload(path: PathBuf, mut form: Form<Upload<'_>>) -> std::io::Result<()
     Ok(())
 }
 
-
-
 #[get("/<path..>")]
 fn root(path: PathBuf) -> RawHtml<String> {
     let mut tera = Tera::default();
+    // TODO: Make this all internal
     tera.add_template_file("./index.html", Some("index.html")).unwrap();
+
     let mut context = Context::new();
+    // TODO
     context.insert("breadcrumbs", &vec![
                    Breadcrumb { name: "foo".to_string(), path: "/no/where/".to_string() }
 
     ]);
-    context.insert("files", &vec![LocalFile{ name: "foo".to_string(), path: "/home/tristan".to_string(), path_type: "File".to_string()}]);
+    // TODO
+    //context.insert("files", &vec![LocalFile{ name: "foo".to_string(), path: "/home/tristan".to_string(), path_type: "File".to_string()}]);
+    context.insert("files", &get_all_files_in_directory(path));
     context.insert("dir_name", "~");
 
     RawHtml(tera.render("index.html", &context).unwrap())
@@ -61,6 +86,7 @@ fn root(path: PathBuf) -> RawHtml<String> {
 
 #[launch]
 fn rocket() -> _ {
+
     rocket::build()
         .mount("/", routes![root])
         .mount("/", routes![upload])
